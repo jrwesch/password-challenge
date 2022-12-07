90ea4af0119baefd85248964c82f69bb68ae5ad4
jrwesch <jrwesch@gmail.com>
2022-12-07 10:26:47 -0500

    got page to look the same as the example. used console logs to see what happens when button is pushed and where info goes. set up comments to give me an idea of what needs to happen and to break up the tasks into smaller chunks.

--- script.js
+++ script.js
@@ -10,3 +10,32 @@ var characterCodes = Array.from(Array(26)).map((_, i) => i + 97);
 var lowercaseLetters = characterCodes.map(code => String.fromCharCode(code));
 var uppercaseLetters = lowercaseLetters.map(letter => letter.toUpperCase());
 
+
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+    console.log("Hey! You clicked the button.");
+
+// 1. prompt user for password criteria
+//      a. password length between 8 and 128
+//      b. lowercase, uppercase, numbers, special characters
+// 2. validate the input
+// 3. generate password based on criteria
+
+
+
+
+
+    return "Generated password will go here.";
+}
+
+// write password to the #password input
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+}
+
+// add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
