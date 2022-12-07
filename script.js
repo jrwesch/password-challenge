cf75053febdfafb0eff31049f6abe5c38dd75e45
jrwesch <jrwesch@gmail.com>
2022-12-07 11:41:08 -0500

    have input from prompts and that input is being stored in variables. need to finish the generator and get those variables into the generator.

--- script.js
+++ script.js
@@ -19,7 +19,30 @@ function generatePassword() {
 // 1. prompt user for password criteria
 //      a. password length between 8 and 128
 //      b. lowercase, uppercase, numbers, special characters
+
+var inputLength = prompt("Choose a password length between 8 and 128 characters");
+    while (inputLength < 8 || inputLength > 128) {
+        alert("Password length must be between 8 and 128");
+        inputLength = prompt("Please choose a password length between 8 and 128");
+    }
+var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
+var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
+var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
+var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
+
 // 2. validate the input
+
+while (confirmUpperCase === false && confirmLowerCase === false && confirmSpecialCharacter === false && confirmNumericCharacter === false) {
+    alert("You must choose at least one parameter");
+    var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
+    var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
+    var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
+    var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters"); 
+}
+
+// log to see what info is being stored
+console.log(inputLength,confirmSpecialCharacter,confirmNumericCharacter,confirmLowerCase,confirmUpperCase);
+
 // 3. generate password based on criteria
 
 
